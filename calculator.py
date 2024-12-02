74e0856f58126c7d5e3c300749a670118aff672b
Stspring <St.spring@tutamail.com>
2024-12-02 17:22:17 +0300

    hggffg

--- calculator.py
+++ calculator.py
@@ -1,105 +1,75 @@
-n1 = int(input('Введите первое число'))
-n2 = int(input('Введите второе число'))
-def sum (n1,n2):
-    return n1 + n2
-def sub (n1, n2):
-    return n1 - n2
-def div (n1,n2):
-    return n1 / n2
-def mul (n1,n2):
-    return n1 * n2
-def calculator(n1, n2, operation):
-    result = None
-    if operation == '+':
-        result = sum(n1, n2)
-    elif operation == '-':
-        result = sub(n1, n2)
-    elif operation == '/':
-        result = div(n1, n2)
-    elif operation == '*':
-        result = mul(n1,n2)
-def operation():
-    mes = input ('Выберите операцию (Введите +, -, /, *)','+ -сложение двух чисел',
-                '- -вычитание двух чисел','/ -умножение двух чисел','* -деление двух чисел')
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
+def add(a):
+    for i in range(1, len(list_num)):
+        list_num[0] += list_num[i]
+    return  list_num[0]
+
+def mul(a):
+    for i in range(1, len(list_num)):
+        list_num[0] *= list_num[i]
+    return list_num[0]
+
+def sub(a):
+    for i in range(1, len(list_num)):
+        list_num[0] -= list_num[i]
+    return list_num[0] 
+
+def div(a):
+    for i in range(1, len(list_num)):
+        list_num[0] /= list_num[i]
+    return list_num[0]
+
+def deg(a):
+    for i in range(1, len(a)):
+        a[0]  = a[0] ** a[i]
+    return a[0]
+
+
+
+
+
+print('Введите пример:')
+example = input()
+list_num = []
+
+if '+' in example:
+    example.replace(' ',' ')
+    spl_ex = example.split('+')
+    list_ex = spl_ex
+    len(list_ex)
+    for i in range(len(list_ex)):
+        list_num.append(float(list_ex[i]))
+    result = add(list_num)
+elif '*' in example:
+    example.replace(' ',' ')
+    spl_ex = example.split('*')
+    list_ex = spl_ex
+    len(list_ex)
+    for i in range(len(list_ex)):
+        list_num.append(float(list_ex[i]))
+    result = mul(list_num)
+elif '-' in example:
+    spl_ex = example.split('-')
+    example.replace(' ',' ')
+    list_ex = spl_ex
+    len(list_ex)
+    for i in range(len(list_ex)):
+        list_num.append(float(list_ex[i]))
+    print(list_num[1])
+    result = sub(list_num)
+elif '/' in example:
+    spl_ex = example.split('/')
+    example.replace(' ',' ')
+    list_ex = spl_ex
+    len(list_ex)
+    for i in range(len(list_ex)):
+        list_num.append(float(list_ex[i]))
+    result = div(list_num)    
+elif '**' in example:
+    spl_ex = example.split('**')
+    example.replace(' ',' ')
+    list_ex = spl_ex
+    len(list_ex)
+    for i in range(len(list_ex)):
+        list_num.append(float(list_ex[i]))
+    result = deg(list_num)
+print(result)
